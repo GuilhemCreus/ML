38a4ee779a0f0f861127fef2dff44b1ec0375f63
Guilhem <ng.creus@gmail.com>
2025-09-06 16:09:41 +0200

    Update Day_5_StandardScaler.py

--- Week 1/Day_5_StandardScaler.py
+++ Week 1/Day_5_StandardScaler.py
@@ -1,1 +1,44 @@
+### Day 5 -- Implementing Standard Scaler for Multi Linear Regression
+"""
+For the last two days, I have been implementing multi linear regression and polynomial regression (which is a case of multi linear regression) without scaling the data which caused some issues if the learning rate wasn't small enough
 
+This is due to the fact that without proper scaling, the gradient descent might go too far following one variable, but too slow following the others
+
+In this code, we will try to implement it to multi linear regression
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+### IMPORT THE DATA
+"""
+First, let's import the dataframe and check that the data is cleaned
+"""
+df = pd.read_csv("...your_path/SalaryUSA.csv")
+
+df.head()
+
+
+"""
+Let's ensure that a relationship exists using a 3D plot
+"""
+plt.close('all')
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+X1 = df['age'].values.tolist()
+X2 = df['experience'].values.tolist()
+Y = df['income'].values.tolist()
+
+ax.scatter(X1, X2, Y, marker='o')
+ax.set_xlabel('age')
+ax.set_ylabel('experience')
+ax.set_zlabel('income')
+
+#plt.show()
+"""
+A relationship kinda exist
+"""
+
+
+### SETTING UP THE ERROR METRIC
