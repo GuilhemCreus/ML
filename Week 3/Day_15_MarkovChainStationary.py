421f3ef219ebf1cfa3e69a96ef5df3a6366ceb32
Guilhem <ng.creus@gmail.com>
2025-09-17 23:58:12 +0200

    Update Day_15_MarkovChainStationary.py

--- Week 3/Day_15_MarkovChainStationary.py
+++ Week 3/Day_15_MarkovChainStationary.py
@@ -8,3 +8,20 @@ import numpy as np
 import random
 
 ### EQUATION SYSTEM
+
+import numpy as np
+
+P = np.array([
+    [0.7, 0.3],
+    [0.5, 0.5]
+])
+
+# Transpose to have the following equation  P.T v = v
+A = P.T - np.eye(2)
+# We add the constraint that the sum of values of v must add up to 1
+A = np.vstack([A, np.ones(2)])
+b = np.array([0, 0, 1])
+
+# We resolve using linalg which is a solver for systems of equations
+stationary = np.linalg.lstsq(A, b, rcond=None)[0]
+print("Stationary distribution:", stationary)
