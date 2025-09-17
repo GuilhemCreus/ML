afe18a35236372512e5023b9f752d64178a0e21d
Guilhem <ng.creus@gmail.com>
2025-09-17 23:56:13 +0200

    Update Day_15_MarkovChainStationary.py

--- Week 3/Day_15_MarkovChainStationary.py
+++ Week 3/Day_15_MarkovChainStationary.py
@@ -1,1 +1,10 @@
+### Day 15 -- Introduction to Markov Chains pt2
+"""
+Yesterday we have seen the basics of Markov Chains
+Today we are going to see the last part that we didn't see yesterday which is solving the equation v = P * v in order to find the stationary vector
+"""
 
+import numpy as np
+import random
+
+### EQUATION SYSTEM
